da6a52cc49d6bea4f9bb90ba575225fcb4b42c38
PavelSergeevichF <40184203+PavelSergeevichF@users.noreply.github.com>
2021-08-13 00:25:41 +0300

    Finish homeworke

---
+++ GBLesson5_Fedotov_P_S/GBLesson5_Fedotov_P_S/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBLesson5_Fedotov_P_S
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Properties.Class1 cl1 = new Properties.Class1();
+            Console.WriteLine("Без использования регулярных выражений");
+            cl1.CheckInputDataNotReg();
+            Console.WriteLine("С использованием регулярных выражений");
+            cl1.CheckInputDataReg();
+
+            Properties.Class2.Message('g');
+        }
+    }
+}
